2a6b144c8a4c044ed75540227091723088d92a87
leilei1238 <397536369@qq.com>
2016-09-06 12:09:50 +0800

    new

---
+++ 公开课第二周/公开课第二周第一天/js1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xiao lei on 2016/8/30.
+ */
+window.onload=function(){
+    var oDiv=document.getElementsByTagName('div')[0];//这里如果没有加[],错的；
+    var ary=new Array();
+    console.dir(oDiv) //innerHTML:元素里面的内容；
+}
